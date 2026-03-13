f55884535b30f12568b34f201a93f37cabe00d2a
Rajdeep Shyam <rshyam@synaptics.com>
2026-03-13 23:13:21 +0530

    udpate build.sh

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ set -e
 
 ROOT_DIR=$(dirname $(readlink -f ${BASH_SOURCE[0]}))
 
+
 docker run -e GITHUB_REF -e GITHUB_REPOSITORY --rm -v /:/host/ -w /host/${ROOT_DIR}/ ghcr.io/syna-astra-dev/synaptics-sphinx-theme/builder:latest
